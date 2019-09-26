02b399f0d6c1e2d4f1ce3262dabe77d040b991db
Loïc Faure-Lacroix <lamerstar@gmail.com>
2019-09-25 20:24:46 -0400

    Fix a bug entrypoint run apt-get update multiple times

--- 12.0-ubuntu/entrypoint.py
+++ 12.0-ubuntu/entrypoint.py
@@ -8,9 +8,11 @@ import sys
 import glob
 import pip
 
-package_list = set()
 
 def pipe(args):
+    """
+    Call the process with std(in,out,err)
+    """
     process = subprocess.Popen(
         args,
         stdin=sys.stdin,
@@ -22,34 +24,78 @@ def pipe(args):
 
     return process.returncode
 
-for packages in glob.glob("/addons/**/apt-packages.txt"):
-    print("Installing packages from %s" % packages)
-    with open(packages, 'r') as pack_file:
-        lines = [line.strip() for line in pack_file]         
-        package_list.update(set(lines))
-    
-    ret = pipe(['apt-get', 'update'])
 
-    if ret != 0:
-        sys.exit(ret)
+def install_apt_packages():
+    """
+    Install debian dependencies.
+    """
+    package_list = set()
 
-    ret = pipe(['apt-get', 'install', '-y'] + list(package_list))
+    for packages in glob.glob("/addons/**/apt-packages.txt"):
+        print("Installing packages from %s" % packages)
+        with open(packages, 'r') as pack_file:
+            lines = [line.strip() for line in pack_file]         
+            package_list.update(set(lines))
+        
+    if len(package_list) > 0:
+        ret = pipe(['apt-get', 'update'])
 
-    if ret != 0:
-        sys.exit(ret)
+        # Something went wrong, stop the service as it's failing
+        if ret != 0:
+            sys.exit(ret)
 
-for requirements in glob.glob("/addons/**/requirements.txt"):
-    print("Installing python packages from %s" % requirements)
-    pip.main(['install', '-r', requirements])
+        ret = pipe(['apt-get', 'install', '-y'] + list(package_list))
 
-quoted_args = [
-    shlex.quote(arg)
-    for arg in sys.argv[1:]
-] or ["true"]
+        # Something went wrong, stop the service as it's failing
+        if ret != 0:
+            sys.exit(ret)
 
-username = "odoo"
 
-# Run the command as odoo while everything is quoted
-ret = pipe(["su", username, "-c", " ".join(quoted_args)])
+def install_python_dependencies():
+    """
+    Install all the requirements.txt file found
+    """
+    # TODO
+    # https://pypi.org/project/requirements-parser/
+    # to parse all the requirements file to parse all the possible specs
+    # then append the specs to the loaded requirements and dump the requirements.txt
+    # file in /var/lib/odoo/requirements.txt and then install this only file
+    # instead of calling multiple time pip
+    for requirements in glob.glob("/addons/**/requirements.txt"):
+        print("Installing python packages from %s" % requirements)
+        pip.main(['install', '-r', requirements])
 
-sys.exit(ret)
+
+def load_secrets():
+    # TODO add a way to load some secrets so odoo process can
+    # use secrets as a way to load passwords/user for postgresql
+    # credentials could also be stored in the HOME of the odoo user
+    # except we cannot rely on secrets 100% because it only works in
+    # swarm mode
+    pass
+
+
+def start():
+    """
+    Main process running odoo
+    """
+    quoted_args = [
+        shlex.quote(arg)
+        for arg in sys.argv[1:]
+    ] or ["true"]
+    username = "odoo"
+
+    # Run the command as odoo while everything is quoted
+    return pipe(["su", username, "-c", " ".join(quoted_args)])
+
+
+def main():
+    install_apt_packages()
+    install_python_dependencies()
+    load_secrets()
+    return start()
+
+
+code = main()
+
+sys.exit(code)
