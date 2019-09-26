69cc0c706bdb2da75879092569bc6ab9104cc8b7
Loïc Faure-Lacroix <lamerstar@gmail.com>
2019-09-25 22:11:56 -0400

    Added scripts

---
+++ build.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import toml
+from datetime import datetime
+from os import path
+import os
+import shutil
+
+FORMAT = "%Y%m%d"
+CUR_DATE = datetime.now().strftime(FORMAT)
+
+templates = {}
+
+def get_template(template_name):
+    if template_name in templates:
+        return templates[template_name]
+
+    template_file = path.join("templates", template_name)
+
+    with open("./templates/Dockerfile.template") as temp_in:
+        template = temp_in.read()
+        templates[template_name] = template
+
+    return template
+
+with open("./versions.toml") as config_in:
+    config = toml.load(config_in)
+
+defaults = config.get('defaults', {})
+
+if path.exists("build"):
+    shutil.rmtree("build")
+
+os.mkdir("build")
+
+tags = []
+
+for tag, config in config.get("odoo", {}).items():
+    config = dict(defaults, **config)
+
+    if 'release' not in config:
+        config['release'] = CUR_DATE
+
+    template = get_template(config.get('template'))
+
+    print("Building version tag %s" % tag)
+
+    os.mkdir(path.join("build", tag))
+
+    with open(path.join("build", tag, "Dockerfile"), "w") as fout:
+        fout.write(template % config)
+
+    tags.append(tag)
+
+for tag in tags:
+    if path.exists(tag):
+        shutil.rmtree(tag)
+    shutil.move(path.join("build", tag), ".")
