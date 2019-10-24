7b8ac0a1577be3d2be6b55956c48718254be18b5
Loïc Faure-Lacroix <lamerstar@gmail.com>
2019-10-24 02:30:33 -0400

    Deploy script updated for version 13

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 docker login
-version=(8 9 10 11 12)
+version=(8 9 10 11 12 13)
 repo="llacroix/odoo"
 
 for version in "${version[@]}"
 do
     cd "${version}.0"
-    docker build -t odoo:${version} .
-    docker tag odoo:${version} $repo:${version}
+    docker build -t local-odoo:${version} .
+    docker tag local-odoo:${version} $repo:${version}
     docker push $repo:${version}
     cd ..
 done
